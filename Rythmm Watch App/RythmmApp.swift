39d73f326eec59825085186960716dd21cb49d8a
Dominik Deren <dominik.deren@live.com>
2024-01-04 12:17:45 +0100

    Initial Commit

---
+++ Rythmm Watch App/RythmmApp.swift
@@ -0,0 +1,17 @@
+//
+//  RythmmApp.swift
+//  Rythmm Watch App
+//
+//  Created by Dominik Deren on 04/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Rythmm_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Rythmm/RythmmApp.swift
@@ -0,0 +1,17 @@
+//
+//  RythmmApp.swift
+//  Rythmm
+//
+//  Created by Dominik Deren on 04/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct RythmmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
